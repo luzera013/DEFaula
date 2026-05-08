39d7f03c33121b4e181c185a74e254e3c0e39f36
luiz <luiz.cerqueira2007@gmail.com>
2026-05-07 22:02:32 -0300

    Add Pessoa class with initialization and display method

--- Def.py
+++ Def.py
@@ -1,1 +1,15 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
 
+    def mostrar(self):
+        print(f"Meu nome é {self.nome} e tenho {self.idade} anos.")
+
+p1 = Pessoa("Ana", 3)
+p2 = Pessoa("Luiz", 18)
+
+p1.mostrar()
+p2.mostrar()
+
+#####################################################################################################################
